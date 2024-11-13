c1a58d8e18248e255df1d123e41cb9d13d0adf35
jmelrita <118337226+jmelrita@users.noreply.github.com>
2024-11-13 15:56:13 +0800

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php #mysqli_connect.php
+DEFINE('DB_USER', 'root');
+DEFINE('DB_PASSWORD', '');
+DEFINE('DB_CHARSET', 'utf8');
+DEFINE('DB_HOST', 'localhost');
+DEFINE('DB_NAME', 'mydb_gabz');
+$conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Could not connect to MySQL: ' . mysqli_connect_error()); 
+?>
+
